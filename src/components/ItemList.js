25d474fde4279ed1ab624047a90d53f8d44e916a
Estela Parrado <estela.parrado@gmail.com>
2018-01-31 19:56:48 +0100

    corrected alt of character with no house

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,13 +3,14 @@ import React, { Component } from 'react';
 class ItemList extends Component {
   render() {
     const backgroundPhoto = {backgroundImage: `url('${this.props.image}')`};
+    const noHouse = this.props.house ? '' : 'hidden';
     const deadOrAlive = this.props.alive ? 'heart-full' : 'heart';
 
     return (
       <li className="items-list">
         <div className="character-photo" style={backgroundPhoto}>
         </div>
-        <img className="house-logo" src={ `Images/${ this.props.house }.png` } alt={ `Logo de la casa ${this.props.house}` }/>
+        <img className={'house-logo ' + noHouse} src={ `Images/${ this.props.house }.png` } alt={ `Logo de la casa ${this.props.house}` }/>
         <h2>{ this.props.name }</h2>
         <h3>{ this.props.birth }</h3>
         <img className={ deadOrAlive } src={`Images/${ deadOrAlive }.svg`} alt="Indicador de vida"/>
